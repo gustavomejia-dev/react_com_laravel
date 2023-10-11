8c323fd52e34b2956054a60d1e887137bab576ee
gustavomejia-dev <ksks_mejia@hotmail.com>
2023-10-11 16:56:24 -0300

    finalizando o componente de notificação

---
+++ frontend/src/components/Notification/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Button, notification, Space } from 'antd';
+
+import { StyledNotification } from './style';
+interface INotification {
+  message: string;
+  visible?: boolean;
+  type: string;
+
+
+}
+type NotificationType = 'success' | 'info' | 'warning' | 'error';
+
+export const Notification = () => {
+      const [api, contextHolder] = notification.useNotification();
+      const openNotificationWithIcon = (type: NotificationType) => {
+        console.log('aquii');
+        api[type]({
+          message: 'Notification Title',
+          description:
+            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        });
+      };
+
+  return (
+    <div>
+      <button onClick={() => openNotificationWithIcon('success')}>CLick</button>
+        {contextHolder}
+    </div>
+  )
+}
---
+++ frontend/src/components/Notification/style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledNotification = styled.div`
+    background-color: #E0E0E0;
+    width: 16rem;
+    height: 80px;
+    border-radius: 0.6rem;
+    span{
+        color: red;
+        text-align: center;
+        align-self: center;
+    }
+    
+
+`
