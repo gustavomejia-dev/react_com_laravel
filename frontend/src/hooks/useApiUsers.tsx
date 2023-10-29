c5c545412f0977d477989f62edc0ca9dfd8c8b9f
gustavomejia-dev <ksks_mejia@hotmail.com>
2023-10-29 18:51:04 -0300

    feat: criando o import de busca nome e email, e criando a tabela aonde mostra os resultados da request

---
+++ frontend/src/hooks/useApiUsers.tsx
@@ -0,0 +1,14 @@
+import axios from "axios"
+import { apiUrl, config } from "./useApi"
+
+export const useApiUsers = () => ({
+    listUsers: async(email: string, name: string) =>{
+
+        const result = await apiUrl.post('/users/list', {email, name}, config)
+        return result.data;
+    }
+    
+
+    
+}    
+)
